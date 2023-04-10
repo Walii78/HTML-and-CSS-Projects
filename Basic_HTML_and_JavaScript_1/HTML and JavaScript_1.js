96c09bf659c453ed154e13a043876bbb61c8153f
Walii78 <129457570+Walii78@users.noreply.github.com>
2023-04-10 15:31:05 -0700

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/HTML and JavaScript_1.js
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+
+<script src="Hello, World.js"></script>
+<script>
+function My_First_Function(){
+var String = "Kiss me, I'm Irish!";
+var result= String.fontcolor("green");
+document.getElementById("Irish").innerHTML=result;
+}
+</script>
+
+</html>
